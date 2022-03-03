30d7ea7de18508265151dcf26774f303857f4f59
trip6love <lanebyers13@gmail.com>
2022-03-03 15:05:21 -0500

    Finishing the service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,3 +17,34 @@ const FILES_TO_CACHE = [
     "./js/idb.js",
     "./js/index.js"
 ];
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+    self.skipWaiting();
+})
+
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            })
+            cacheKeeplist.push(CACHE_NAME);
+            return Promise.all(
+                keyList.map(function (key, i) {
+                    if (cacheKeeplist.indexOf(key) === -1) {
+                        console.log('deleting cache : ' + keyList[i]);
+                        return caches.delete(keyList[i])
+                    }
+                })
+            )
+        })
+    )
+    self.clients.claim();
+})
+
