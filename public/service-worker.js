61c7a0cde88b0e1d144c039c60557ac69d4fe4eb
trip6love <lanebyers13@gmail.com>
2022-03-05 13:33:38 -0500

    finished service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -48,3 +48,40 @@ self.addEventListener('activate', function(e) {
     self.clients.claim();
 })
 
+self.addEventListener('fetch', function(event) {
+    if (event.request.url.includes('/api/')) {
+      event.respondWith(
+        caches
+          .open(DATA_CACHE_NAME)
+          .then(cache => {
+            return fetch(event.request)
+              .then(response => {
+                if (response.status === 200) {
+                  cache.put(event.request.url, response.clone());
+                }
+  
+                return response;
+              })
+              .catch(err => {
+                return cache.match(event.request);
+              });
+          })
+          .catch(err => console.log(err))
+      );
+  
+      return;
+    }
+  
+    event.respondWith(
+      fetch(event.request).catch(function() {
+        return caches.match(event.request).then(function(response) {
+          if (response) {
+            return response;
+          } else if (event.request.headers.get('accept').includes('text/html')) {
+            return caches.match('/');
+          }
+        });
+      })
+    );
+  });
+
